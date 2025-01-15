4abea1ccfcc00980b91eac0e259f7994b68b6cff
AkshitaTomar1 <axtomar7@gmail.com>
2025-01-15 20:14:11 +0530

    template created

--- template.py
+++ template.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 package_name="WineQualityPrediction"
 
@@ -7,42 +11,47 @@ list_of_files=[
     "github/workflows/.gitkeep",
     f"src/{package_name}/__init__.py",
     f"src/{package_name}/components/__init__.py",
-    f"src/{package_name}/components/data_ingestion.py",
-    f"src/{package_name}/components/data_transformation.py",
-    f"src/{package_name}/components/model_trainer.py",
-    f"src/{package_name}/pipelines/__init__.py",
-    f"src/{package_name}/pipelines/training_pipeline.py",
-    f"src/{package_name}/pipelines/prediction_pipeline.py",
-    f"src/{package_name}/logger.py",
-    f"src/{package_name}/exception.py",
     f"src/{package_name}/utils/__init__.py",
-    "notebooks/research.ipynb",
-    "notebooks/data/.gitkeep",
-    "requirements.txt",
+    f"src/{package_name}/utils/common.py",
+    f"src/{package_name}/config/__init__.py",
+    f"src/{package_name}/config/configuration.py",
+    f"src/{package_name}/pipeline/__init__.py",
+    f"src/{package_name}/entity/__init__.py",
+    f"src/{package_name}/entity/config_entity.py",
+    f"src/{package_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    
+    "main.py",
+    "Dockerfile",
     "setup.py",
-    "init_setup.sh",
+    "research/research.ipynb",
+    "templates/index.html"
 ]
 
 
-# here will create a directory
+
+
 
 for filepath in list_of_files:
-    filepath=Path(filepath)
-    filedir,filename=os.path.split(filepath)
-    
-    """ how exist_ok works:if "directory" already exists, 
-    os.makedirs() will not raise an error, and it will do nothing. 
-    If "my_directory" doesn't exist, it will create the directory.
-    """
-    if filedir != "":
-        os.makedirs(filedir,exist_ok=True)
-        
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,"w") as f:
-            pass
-    else:
-        print("file already exists")
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
 
-# here will use the file handling
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
+
 
 
